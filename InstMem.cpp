5b162502928f4818a1859ba9d91f06a7f29063fb
hysamah <57324118+hysamah@users.noreply.github.com>
2020-10-29 16:21:19 +0200

    separated instruction memory implemetation

---
+++ InstMem.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "InstMem.h"
+using namespace std;
+
+InstMem::InstMem()
+{
+	this->Inst = new Instruction*[1024];
+	this->allocated = 0;
+}
+
+Instruction* InstMem::getInst(int p) {
+	if (p <= maxPc)
+		return Inst[p];
+	else cout << "Invalid PC value! \n";
+
+}
+void InstMem::allocate(Instruction& newInst) {
+	if (this->allocated == 1024)
+		throw overflow_error("Unable to allocate new space for a new instruction, size reached 1024");
+	//cout << "SIZ: " << inst.pars.size() << endl;
+	this->Inst[this->allocated] = &inst;
+	this->allocated++;
+}
+
+void InstMem::print() {
+	for (Instruction *x : Inst) {
+		x->print();
+	}
+}
+InstMem::~InstMem() {
+	for (int i = 0; i < this->allocated; i++) {
+		delete this->Inst[i];
+	}
+	delete[] this->Inst;
+}
