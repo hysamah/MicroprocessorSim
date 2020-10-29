5ffc6fe5ac755b77f90428f1eb66e8782366b7eb
hysamah <57324118+hysamah@users.noreply.github.com>
2020-10-29 17:28:02 +0200

    Update InstMem.cpp

--- InstMem.cpp
+++ InstMem.cpp
@@ -18,14 +18,19 @@ void InstMem::allocate(Instruction& newInst) {
 	if (this->allocated == 1024)
 		throw overflow_error("Unable to allocate new space for a new instruction, size reached 1024");
 	//cout << "SIZ: " << inst.pars.size() << endl;
-	this->Inst[this->allocated] = &Inst;
+	this->Inst[this->allocated] = &newInst;
 	this->allocated++;
 }
 
 void InstMem::print() {
-	for (Instruction *x : Inst) {
-		x->print();
+	for (int i = 0; i < this->allocated; i++)
+	{
+		Inst[i]->print();
 	}
+	
+	/*for (Instruction *x : Inst) {
+		x->print();
+	}*/
 }
 InstMem::~InstMem() {
 	for (int i = 0; i < this->allocated; i++) {
